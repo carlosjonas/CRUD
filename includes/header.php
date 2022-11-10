edf16d6351f73f1df4d50ed22dc007048b4ad591
Carlos Jonas <jonas.fonseca@opovodigital.com>
2022-11-09 22:48:54 -0300

    feature: criando o footer,header e listagem do meu index

---
+++ includes/header.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Cadastro de Usuários</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container">
+        <nav class="navbar navbar-dark bg-dark fixed-top">
+          <div class="container">
+            <a class="navbar-brand" href="#">Cadastro de Usuários</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasDarkNavbar" aria-controls="offcanvasDarkNavbar">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="offcanvas offcanvas-end text-bg-dark" tabindex="-1" id="offcanvasDarkNavbar" aria-labelledby="offcanvasDarkNavbarLabel">
+              <div class="offcanvas-header">
+                <h5 class="offcanvas-title" id="offcanvasDarkNavbarLabel">Usuários</h5>
+                <button type="button" class="btn-close btn-close-white" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+              </div>
+              <div class="offcanvas-body">
+                <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Ativos</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="#">Inativos</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </nav>
---
+++ includes/listagem.php
@@ -0,0 +1,11 @@
+	
+
+	<main>
+		<div class="container mt-5 pt-5">
+			<section>
+				<a href="cadastrar.php">
+					<button class="btn btn-success">Novo usuário</button>
+				</a>
+			</section>
+		</div>
+	</main>
