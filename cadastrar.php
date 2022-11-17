e2911350f8a842bc7392aacff09c68beb193ac3e
Carlos Jonas <jonas.fonseca@opovodigital.com>
2022-11-17 20:31:55 -0300

    refactor: adicionando a validação dos dados por meio do post

--- cadastrar.php
+++ cadastrar.php
@@ -2,11 +2,24 @@
 	
 	require __DIR__.'/vendor/autoload.php';
 
-	echo "<pre>"; print_r($_POST);echo "</pre>";
+	use \App\Entity\Usuario;
+
+	
 
 	//Validação do POST
 	if (isset($_POST['nome'],$_POST['email'],$_POST['rg'],$_POST['cpf'],$_POST['telefone'],$_POST['ativo'])) {
-		exit('Cadastrar');
+		$usuario = new Usuario;
+		$usuario->nome 	   = $_POST['nome'];
+		$usuario->email    = $_POST['email'];
+		$usuario->rg 	   = $_POST['rg'];
+		$usuario->cpf 	   = $_POST['cpf'];
+		$usuario->telefone = $_POST['telefone'];
+		$usuario->ativo    = $_POST['ativo'];
+		
+		//echo "<pre>"; print_r($usuario);echo "</pre>";
+
+		$usuario->cadastrar();
+
 	}
 
 	include __DIR__.'/includes/header.php';
