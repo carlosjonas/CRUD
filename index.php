7316eec65a0f600511e1ed0df810fa0a8f1da840
Carlos Jonas <jonas.fonseca@opovodigital.com>
2022-11-18 21:36:07 -0300

    refactor: instanciando e chamando metodo getUsuarios

--- index.php
+++ index.php
@@ -2,6 +2,10 @@
 	
 	require __DIR__.'/vendor/autoload.php';
 	
+	use \App\Entity\Usuario;
+
+	$usuarios = Usuario::getUsuarios();
+
 	include __DIR__.'/includes/header.php';
 	include __DIR__.'/includes/listagem.php';
 	include __DIR__.'/includes/footer.php';
