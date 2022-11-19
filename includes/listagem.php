463a366d08a701de6fc952d0b791b9bac8133c6e
Carlos Jonas <jonas.fonseca@opovodigital.com>
2022-11-18 21:38:40 -0300

    refactor: criando a estrutura de tabela e retornando as informações dos usuarios

--- includes/listagem.php
+++ includes/listagem.php
@@ -1,4 +1,19 @@
-	
+	<?php 
+
+	$resultados = '';
+	foreach ($usuarios as $usuario) {
+		$resultados .= '<tr>
+							<td>' .$usuario->id. '</td>
+							<td>' .$usuario->nome. '</td>
+							<td>' .$usuario->email. '</td>
+							<td>' .$usuario->rg. '</td>
+							<td>' .$usuario->cpf. '</td>
+							<td>' .$usuario->telefone. '</td>
+							<td>' .($usuario->ativo == '1' ? 'Sim' : 'Não'). '</td>
+							<td></td>
+						</tr>';
+	}
+	 ?>
 
 	<main>
 		<div class="container mt-5 pt-5">
@@ -7,5 +22,26 @@
 					<button class="btn btn-success">Novo usuário</button>
 				</a>
 			</section>
+
+			<section>
+				<table class="table mt-3">
+					<thead>
+						<tr>
+							<th>Id</th>
+							<th>Nome</th>
+							<th>Email</th>
+							<th>RG</th>
+							<th>CPF</th>
+							<th>Telefone</th>
+							<th>Ativo</th>
+							<th>Ações</th>
+						</tr>
+					</thead>
+					<tbody>
+						<?= $resultados; ?>
+					</tbody>
+				</table>	
+			</section>
+			
 		</div>
 	</main>
