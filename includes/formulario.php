4bb9a88590d5f4e6342ec1ce071aa6093f695c18
Carlos Jonas <jonas.fonseca@opovodigital.com>
2022-11-30 21:50:35 -0300

    refactor: adicionando as informações dos usuarios ao atributo value do formulario

--- includes/formulario.php
+++ includes/formulario.php
@@ -1,3 +1,7 @@
+<?php 
+
+use \App\Entity\Usuario;
+ ?>
 	<main>
 		<div class="container mt-5 pt-5">
 			<section>
@@ -6,32 +10,62 @@
 				</a>
 			</section>
 
-			<h2 class="mt-3">Cadastrar Usuário</h2>
+			<h2 class="mt-3"><?= TITLE ?></h2>
 
 			<form method="POST">
 				<div class="form-group mt-3">
 					<label>Nome</label>
-					<input type="text" class="form-control" name="nome" minlength="8" maxlength="255" required>
+					<input type="text" class="form-control" name="nome" minlength="8" maxlength="255" value="<?php if (@$usuario != 'null') {
+									echo @$usuario->nome;			
+								 }else{
+									echo "";
+								 } ?>
+							"required>
+
 				</div>
 
 				<div class="form-group mt-3">
 					<label>Email</label>
-					<input type="email" class="form-control" name="email" minlength="12" maxlength="255" required>
+					<input type="email" class="form-control" name="email" minlength="12" maxlength="255" 
+					value="<?php if (@$usuario != 'null') {
+									echo @$usuario->email;			
+								 }else{
+									echo "";
+								 } ?>
+							"required>
 				</div>
 
 				<div class="form-group mt-3">
 					<label>RG</label>
-					<input type="text" class="form-control" name="rg" minlength="12" maxlength="12" required>
+					<input type="text" class="form-control" name="rg" minlength="12" maxlength="12" 
+					value="<?php if (@$usuario != 'null') {
+									echo @$usuario->rg;			
+								 }else{
+									echo "";
+								 } ?>
+							"required>
 				</div>
 
 				<div class="form-group mt-3">
 					<label>CPF</label>
-					<input type="text" class="form-control" name="cpf" minlength="14" maxlength="14" required>
+					<input type="text" class="form-control" name="cpf" minlength="14" maxlength="14" 
+					value="<?php if (@$usuario != 'null') {
+									echo @$usuario->cpf;			
+								 }else{
+									echo "";
+								 } ?>
+							"required>
 				</div>
 
 				<div class="form-group mt-3">
 					<label>Telefone</label>
-					<input type="text" class="form-control" name="telefone" minlength="14" maxlength="14" required>
+					<input type="text" class="form-control" name="telefone" minlength="14" maxlength="14" 
+					value="<?php if (@$usuario != 'null') {
+									echo @$usuario->telefone;			
+								 }else{
+									echo "";
+								 } ?>
+							"required>
 				</div>
 
 				<div class="form-group mt-3">
@@ -46,7 +80,17 @@
 
 						<div class="form-check form-check-inline">
 							<label class="form-control">
-								<input type="radio" name="ativo" value="0"> Inativo
+								<input type="radio" name="ativo" value="0" 
+									<?php //$usuario->ativo == '0' ? 'checked' : '' 
+										if (@$usuario != 'null') {
+											if (@$usuario->ativo == '0') {
+												echo "checked";
+											}else{
+												echo "";
+											}
+										}?>> 
+
+								Inativo
 							</label>
 						</div>
 					</div>
@@ -56,9 +100,5 @@
 					<button class="btn btn-success" type="submit">Enviar</button>
 				</div>
 			</form>
-
-
-
-
 		</div>
 	</main>
