631bdc13aeb5b79331d993cd452e7604b0f8acfb
Carlos Jonas <jonas.fonseca@opovodigital.com>
2023-01-03 20:58:28 -0300

    feature: adicionando a página de excluir

---
+++ excluir.php
@@ -0,0 +1,37 @@
+<?php 
+	
+	require __DIR__.'/vendor/autoload.php';
+
+	use \App\Entity\Usuario;
+
+	//Validação do id
+	if (!isset($_GET['id']) or !is_numeric($_GET['id'])) {
+		header('location: index.php?status=error');
+		exit;
+	}
+
+	//Consultando a vaga para editar
+	$usuario = Usuario::getUsuario($_GET['id']);
+	
+	//Validação da vaga
+	if (!$usuario instanceof Usuario) {
+		header('location: index.php?status=error');
+		exit;				
+	}
+
+	//Validação do POST
+	if (isset($_POST['excluir'])) {
+		//echo "<pre>"; print_r($usuario); echo "</pre>"; exit;
+
+		$usuario->excluir();
+
+		header('location: index.php?status=success');
+		exit;
+	}
+
+	include __DIR__.'/includes/header.php';
+	include __DIR__.'/includes/confirmar-exclusao.php';
+	include __DIR__.'/includes/footer.php';
+
+
+ ?>
