88024b363c3c857bc2734d27a45df0068d023ce5
Carlos Jonas <jonas.fonseca@opovodigital.com>
2023-01-05 21:04:43 -0300

    feature: adicionando links para as páginas ativos e inativos

--- includes/header.php
+++ includes/header.php
@@ -22,10 +22,13 @@
               <div class="offcanvas-body">
                 <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
                   <li class="nav-item">
-                    <a class="nav-link active" aria-current="page" href="#">Ativos</a>
+                    <a class="nav-link" aria-current="page" href="index.php">Geral</a>
                   </li>
                   <li class="nav-item">
-                    <a class="nav-link" href="#">Inativos</a>
+                    <a class="nav-link" aria-current="page" href="ativos.php">Ativos</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="inativos.php">Inativos</a>
                   </li>
                 </ul>
               </div>
